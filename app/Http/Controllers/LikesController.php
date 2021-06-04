f581bc0424728322085adfc049da076e1e379fc1
Gianni Errera <giannierrera@yahoo.it>
2021-06-04 09:49:19 +0200

    Removing records from likes table when false values
    	Now if both 'liked' and 'dislike' fields are set to false,
    	likes table record is deleted, this way table should be cleanere
    	and it wouldn't need pruning of orphaned records
    
    	Added a migration for comments, as I think it would be nice to
    	reply to tweets

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -1,19 +1,37 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Like;
 
 use Illuminate\Http\Request;
 use App\Models\Tweet;
+use DB;
 
 class LikesController extends Controller
 {
     public function like(Tweet $tweet, $liked=false) {
-        $tweet->like($liked);
+        $like = DB::table('likes')
+        ->where('tweet_id', $tweet->id)
+        ->where('user_id', current_user()->id)
+        ->where('liked', true);
+        if ($like->exists()) {
+                $like->delete();
+        } else {
+            $tweet->like($liked);
+        }
         return back();
     }
 
     public function dislike(Tweet $tweet, $disliked=false) {
-        $tweet->dislike($disliked);
+        $dislike = DB::table('likes')
+        ->where('tweet_id', $tweet->id)
+        ->where('user_id', current_user()->id)
+        ->where('disliked', true);
+        if ($dislike->exists()) {
+                $dislike->delete();
+        } else {
+            $tweet->dislike($disliked);
+        }
         return back();
     }
 }
